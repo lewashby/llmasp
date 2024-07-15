2af4d3af3ad5c858b9f2e7f0503bd2be4a8af22b
Luis Angel <luis.rodriguezreiners@gmail.com>
2024-07-15 10:02:54 +0200

    separated knowledge base from encoding in different files

--- llmasp/llm/LLMASP.py
+++ llmasp/llm/LLMASP.py
@@ -9,6 +9,8 @@ class LLMASP(AbstractLLMASP):
     def __init__(self, config_file: str, behavior_file: str, behavior: str, llm: LLMHandler, solver):
         super().__init__(config_file, behavior_file, llm, solver)
         self.behavior = self.behaviors[behavior]
+        db_file = self.load_file(self.config["knowledge_base_file"])
+        self.database = db_file["database"]
 
     def load_file(self, config_file: str):
         return yaml.load(open(config_file, "r"), Loader=yaml.Loader)
@@ -52,47 +54,52 @@ class LLMASP(AbstractLLMASP):
         context = self.behavior["natural-language"]["context"]
         responses = []
         _, format = self.__get_property(self.config["postprocessing"], "_")
-        _, final_response = self.__get_property(self.config["postprocessing"], "summary")
-        _, fact_translation = self.__get_property(self.config["postprocessing"], "translation")
+        _, final_response = self.__get_property(self.config["postprocessing"], "summarize")
+        _, failed_translation = self.__get_property(self.config["postprocessing"], "failed")
         real_context = re.sub(r"\{format\}", format, context)
+        print(grouped_facts)
         for fact_name in grouped_facts:
+            _, fact_translation = self.__get_property(self.config["postprocessing"], "translation")
             f_translation_key, f_translation_value = self.__get_property(self.config["postprocessing"], fact_name, is_fact=True)
-            fact_translation = re.sub(r"\{fact\}", fact_translation, f_translation_key)
-            fact_translation = re.sub(r"\{meaning\}", fact_translation, f_translation_value)
+            fact_translation = re.sub(r"\{fact\}", f_translation_key, fact_translation)
+            fact_translation = re.sub(r"\{meaning\}", f_translation_value, fact_translation)
             res = self.llm.call([*queries, *[
                     self.__prompt("system", real_context),
                     self.__prompt("system", fact_translation),
                     self.__prompt("user", f"[FACTS]{"\n".join(grouped_facts[fact_name])}[/FACTS]"),
                 ]])
             responses.append(res)
+            print("------------------------------------------------")
             print("fact: ", fact_name)
             print(res)
+        if (len(responses) > 0):
+            final_response = re.sub(r"\{response\}", "\n".join(responses), final_response)
+            out = [self.__prompt("system", real_context), self.__prompt("system", final_response)]
             print("------------------------------------------------")
-        final_response = re.sub(r"\{response\}", final_response, "\n".join(responses))
-        out = [self.__prompt("user", final_response)]
+        else:
+            out = [self.__prompt("system", failed_translation)]
         return self.llm.call(out)
 
     def natural_to_asp(self, user_input:str):
         queries = self.__create_queries(user_input)
-        asp_input = ""
+        created_facts = ""
         for q in queries.values():
             facts = self.llm.call(q)
             facts = re.findall(r"\b[a-zA-Z][\w_]*\([^)]*\)\.", facts)
             facts = "\n".join(facts)
-            asp_input = f"{asp_input}\n{facts}"
+            created_facts = f"{created_facts}\n{facts}"
             q.append(self.__prompt("assistant", facts))
-        print(facts)
-        knowledge_base = self.config["knowledge_base"]
-        asp_input = f"{asp_input}\n{knowledge_base}"
+        asp_input = f"{created_facts}\n{self.database}\n{self.config["encoding"]}"
 
-        return asp_input, queries
+        return created_facts, asp_input, queries
 
     def run(self):
         user_input = input("input: ")
-        asp_input, history = self.natural_to_asp(user_input)
+        created_facts, asp_input, history = self.natural_to_asp(user_input)
+        print(created_facts)
 
         result, _, _ = self.solver.solve(asp_input)
         print(result)
 
-        response = self.asp_to_natural({}, result)
+        response = self.asp_to_natural(history, result)
         print(response)
