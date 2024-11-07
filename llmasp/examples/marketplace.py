f4721c38b07326c6e69fa269c2ed8d6c634466f5
Luis Angel <luis.rodriguezreiners@gmail.com>
2024-11-07 15:28:44 +0100

    updated example

--- llmasp/examples/marketplace.py
+++ llmasp/examples/marketplace.py
@@ -5,5 +5,5 @@ def marketplace_example(model, server):
     user_input = "I would like some cooking ideas for a dessert with apples and for a main plate with meat."
     llm = LLMHandler(model, server)
     solver = Solver()
-    llmasp = LLMASP("./specifications/application_marketplace.yml", "./specifications/behavior_translator.yml", llm, solver)
+    llmasp = LLMASP("./specifications/application_marketplace.yml", "./specifications/behavior_translator_v2.yml", llm, solver)
     llmasp.run(user_input, verbose=1)
