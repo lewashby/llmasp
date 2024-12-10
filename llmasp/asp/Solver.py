b697c3c58338396408d156b7188c1b6f1606a782
Luis Angel <luis.rodriguezreiners@gmail.com>
2024-12-10 12:18:54 +0100

    solver solve function context as parameter

--- llmasp/asp/Solver.py
+++ llmasp/asp/Solver.py
@@ -11,7 +11,7 @@ class Context:
 
 class Solver:
 
-    def solve(self, program, arguments=["--opt-strategy=usc,k,0,5" ,"--opt-usc-shrink=rgs"], timeout=2):
+    def solve(self, program, arguments=["--opt-strategy=usc,k,0,5" ,"--opt-usc-shrink=rgs"], timeout=2, context=Context):
 
         results: list = []
         handle = None
@@ -20,7 +20,7 @@ class Solver:
         
         control = Control(arguments, logger=logger)
         control.add(f"{program}")
-        control.ground([("base", [])], context=Context)
+        control.ground([("base", [])], context=context)
 
         with control.solve(on_model=on_model, async_=True) as handle:
             handle.wait(timeout)
